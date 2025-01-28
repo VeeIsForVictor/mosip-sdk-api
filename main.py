c7c4243676395801ceae7c88e6b3abbf9bed85db
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-28 15:35:55 +0800

    feat: add pydantic models for request bodies

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 server = FastAPI()
+
+
+class DemographicRequestData(BaseModel):
+    uin: str
+
+
+class AgeRequest(DemographicRequestData):
+    age: int
+
+
+class DobRequest(DemographicRequestData):
+    dob: str
 
 
 @server.get("/")
