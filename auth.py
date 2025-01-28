a1508fdf62d581ca90838c4c9c1a3c043c29a587
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-28 15:32:12 +0800

    feat: add basic test to main() for auth.py

--- auth.py
+++ auth.py
@@ -12,11 +12,24 @@ class AuthHandler:
     def yesno(self, uid: str, demographic_data: DemographicsModel) -> bool:
         response: Response = self.authenticator.auth(  # type: ignore
             individual_id=uid,
-            individual_id_type="uid",
+            individual_id_type="UIN",
             consent=True,
             demographic_data=demographic_data,
         )
 
         response_body: dict[str, str] = response.json()
         response_proper: dict = response_body.get("response")
-        return response_proper.get("auth_status")
+        assert response_proper is not None
+        final_response = response_proper.get("authStatus")
+        assert final_response is not None
+        return final_response
+
+
+def main():
+    auth = AuthHandler()
+    demo: DemographicsModel = DemographicsModel(dob="1992/04/29")
+    print(auth.yesno("2047631038", demo))
+
+
+if __name__ == "__main__":
+    main()
